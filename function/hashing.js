c2dc264eccf14507bb90b9573dfffd3bc55a60bd
Shahriar Kabir Hemal <shahriar12688@gmail.com>
2024-12-18 23:15:14 +0600

    login signup done

--- function/hashing.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require('bcrypt');
-const hashingPass = async (password) => {
-  const hashPassword = await bcrypt.hash(password, 14);
-  return hashPassword;
-};
-module.exports = hashingPass;
